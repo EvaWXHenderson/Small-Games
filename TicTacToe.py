2bc62af0b2bf45c97822d35fba8a8cadf8b04a6d
Eva Henderson <eva.henderson01@gmail.com>
2025-08-21 17:02:20 +0100

    Update TicTacToe.py
    no re-selection
    time delay before quit()

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from matplotlib.animation import FuncAnimation
-
+import time
 import tkinter as tk
 
 grid_size = 60
@@ -30,6 +30,33 @@ def grid(size = grid_size):
         Z[30][38] = 3
     return Z
 
+def define_box(x, y):
+    if x<20:
+        if y < 20:
+            box = grid_boxes[2][0]
+        elif y < 40 and y > 20:
+            box = grid_boxes[2][1]
+        elif y < 60 and y > 40:
+            box = grid_boxes[2][2]
+
+    if x<40 and x>20:
+        if y < 20:
+            box = grid_boxes[1][0]
+        elif y < 40 and y > 20:
+            box = grid_boxes[1][1]
+        elif y < 60 and y > 40:
+            box = grid_boxes[1][2]
+
+    if x<60 and x>40:
+        if y < 20:
+            box = grid_boxes[0][0]
+        elif y < 40 and y > 20:
+            box = grid_boxes[0][1]
+        elif y < 60 and y > 40:
+            box = grid_boxes[0][2]
+
+    return box
+
 def grid_update(x, y):
     global grid_boxes, Z, turn
 
@@ -40,7 +67,7 @@ def grid_update(x, y):
        colour = 2
     if turn%2 != 0 :
         colour = 3
-
+    
     if x<20:
         if y < 20:
             grid_boxes[2][0] = colour
@@ -71,42 +98,54 @@ def print_winner(player):
     root = tk(screenName=None, baseName=None, className='tk', useTk=1)
 
 def check_win():
-    twos = 0
-    threes = 0
+    draw = True
 
     for row in grid_boxes:
         if row[0]==3 and row[1]==3 and row[2]==3:
             print('PLAYER 1 - WIN!')
+            time.sleep(1)
             quit()
         elif row[0]==2 and row[1]==2 and row[2]==2:
             print('PLAYER 2 - WIN!')
-            print('faulty - 1')
+            time.sleep(1)
             quit()
     
     for i in range(3):
         if grid_boxes[0][i]==3 and grid_boxes[0][i]==grid_boxes[1][i] and grid_boxes[1][i]==grid_boxes[2][i]:
             print('PLAYER 1 - WIN!')
+            time.sleep(1)
             quit()
         if grid_boxes[0][i]==2 and grid_boxes[0][i]==grid_boxes[1][i] and grid_boxes[1][i]==grid_boxes[2][i]:
             print('PLAYER 2 - WIN!')
-            print('faulty - 2')
+            time.sleep(1)
             quit()
     
     if grid_boxes[0][0]==3 and grid_boxes[0][0]==grid_boxes[1][1] and grid_boxes[1][1]==grid_boxes[2][2]:
         print('PLAYER 1 - WIN!')
+        time.sleep(1)
         quit()
     elif grid_boxes[0][0]==2 and grid_boxes[0][0]==grid_boxes[1][1] and grid_boxes[1][1]==grid_boxes[2][2]:
         print('PLAYER 2 - WIN!')
-        print('faulty - 3')
+        time.sleep(1)
         quit()
 
 
     if grid_boxes[0][2]==3 and grid_boxes[0][2]==grid_boxes[1][1] and grid_boxes[1][1] ==grid_boxes[2][0]:
         print('PLAYER 1 - WIN!')
+        time.sleep(1)
         quit()
     elif grid_boxes[0][2]==2 and grid_boxes[0][2]==grid_boxes[1][1] and grid_boxes[1][1] ==grid_boxes[2][0]:
         print('PLAYER 2 - WIN!')
-        print('faulty - 4')
+        time.sleep(1)
+        quit()
+
+    for row in grid_boxes:
+        for i in range(3):
+            if row[i] == 0:
+                draw = False
+    if draw == True:
+        print('DRAW!!!')
+        time.sleep(1)
         quit()
 
 def Z_update(size = grid_size, grid = grid_boxes, coords = grid_coords):
@@ -150,12 +189,15 @@ def anim_data(frame):
 
 def select(event):
     global ix, iy, grid_boxes, turn
-
-    turn += 1 #if turn odd => player1, if turn even => player2
-
     ix, iy= round(event.xdata), round(event.ydata)
 
-    grid_boxes = grid_update(x = ix, y = iy)
+    box = define_box(x = ix, y = iy)
+    
+    if box == 0:
+        turn += 1 #if turn odd => player1, if turn even => player2
+        grid_boxes = grid_update(x = ix, y = iy)
+    else:
+        pass
 
 
 
@@ -170,7 +212,7 @@ fig.canvas.mpl_connect('button_press_event', select)
 image = ax.imshow(grid(), origin = 'upper', cmap=cmap)
 image.set_data(Z_update())
 
-ani = FuncAnimation(fig, anim_data, frames = 100, interval = 100, blit = False)
+ani = FuncAnimation(fig, anim_data, frames = 100, interval = 10, blit = False)
 plt.show()
 
 print(grid_boxes)
